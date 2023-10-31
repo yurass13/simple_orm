9f1ad0e9841ada83ba122225b9973d1b7d87f0c6
Yury Stolpov <yurass130@gmail.com>
2023-10-31 04:17:10 +0300

    Fix: stylefixes ModelBase -> Model

--- simple_orm/models.py
+++ simple_orm/models.py
@@ -3,17 +3,18 @@ from dataclasses import dataclass
 from typing import TypeVar, Generic
 
 
-M = TypeVar('M', bound='ModelBase')
+M = TypeVar('M', bound='Model')
 
 class ForeignKey(Generic[M]):
-    # TODO need more info and checks
+    # TODO need more info and checks                                                ????????
     def __init__(self, model:M) -> None:
         self._related_model = model
 
     def __repr__(self) -> str:
         return str(self._related_model.id)
 
-
+# TODO try use metaclass instead of dataclass decorator                             IMPORTANT
+# TODO try create a new solution based on Fields family and metaclass ModelCreator  FUTURE IDEA
 @dataclass
-class ModelBase:
+class Model:
     id: int
